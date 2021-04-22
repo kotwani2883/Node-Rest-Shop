37c1c644fef72f9b395666b68aa9664180ab4687
Palak <palakkotwani2883@gmail.com>
2021-04-22 13:14:49 +0530

    Added User SignUp Functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose=require('mongoose');
 
 const productRoutes=require('./api/routes/products')
 const orderRoutes=require('./api/routes/orders')
+const userRoutes=require('./api/routes/user')
 mongoose.connect('mongodb+srv://pk22:' + process.env.MONGO_ATLAS_PW +'@noderestshop.k4ioz.mongodb.net/NODE-REST-SHOP?retryWrites=true&w=majority',{useNewUrlParser: true})
 
 mongoose.Promise=global.Promise;
@@ -25,6 +26,7 @@ app.use((req,res,next)=>{
 })
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes)
+app.use('/user',userRoutes)
 
 app.use((req,res,next)=>{
     const error=new Error('Not Found');
