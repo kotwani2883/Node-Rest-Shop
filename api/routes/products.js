568056c9f4bd5814bcbf4707382fa398a73ff9f5
Palak <palakkotwani2883@gmail.com>
2021-04-23 11:03:24 +0530

    Protected Routes added!!

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer=require("multer");
 const path = require('path');
+const checkAuth=require('../middleware/check-auth')
 
 const storage=multer.diskStorage({
  destination: function(req,res,cb){
@@ -66,7 +67,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", upload.single('productImage'),checkAuth,(req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
